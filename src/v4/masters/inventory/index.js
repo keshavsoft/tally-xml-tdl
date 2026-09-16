b1ffc4dab68ead45584a677216d38a1e615cc495
keshavsoft <nknnkumar@yahoo.com>
2026-09-17 02:36:12 +0530

    batch perfect

--- src/v4/masters/inventory/index.js
+++ src/v4/masters/inventory/index.js
@@ -2,15 +2,18 @@ export { default as godown, body as godownBody, get as getGodown } from "./godow
 export { default as stockCategory, body as stockCategoryBody, get as getStockCategory } from "./stockCategory/index.js";
 export { default as uom, body as uomBody, get as getUom } from "./uom/index.js";
 export { default as stockItems, body as stockItemsBody, get as getStockItems } from "./stockItems/index.js";
+export { default as batch, body as batchBody, get as getBatch } from "./batch/index.js";
 
 import godown from "./godown/index.js";
 import stockCategory from "./stockCategory/index.js";
 import uom from "./uom/index.js";
 import stockItems from "./stockItems/index.js";
+import batch from "./batch/index.js";
 
 export default {
     godown,
     stockCategory,
     uom,
-    stockItems
+    stockItems,
+    batch
 };
