e13dbcf8fa55c0702bccdfea1af99231f995d65c
keshavsoft <nknnkumar@yahoo.com>
2026-09-17 01:29:49 +0530

    purc

--- Test/v11/asArray/transactions/purchase.js
+++ Test/v11/asArray/transactions/purchase.js
@@ -1,6 +1,25 @@
-import { transactions } from "../../../src/v4/index.js";
+import { transactions } from "../../../../src/v4/index.js";
 import { saveOutput } from "../common/index.js";
 
 const data = await transactions.purchase();
-saveOutput({ callerFile: import.meta.url, inData: data });
+
+const asSimpleArray = data.VOUCHER.map(element => {
+    return {
+        name: element.DATE["#text"],
+        voucherNumber: element.VOUCHERNUMBER,
+        voucherTypeName: element.VOUCHERTYPENAME,
+        partyLedgerName: element.PARTYLEDGERNAME["#text"],
+        reference: element.REFERENCE?.["#text"],
+        isDeleted: element.ISDELETED,
+        isDeemedPositive: element.ISDEEMEDPOSITIVE["#text"],
+        effectiveDate: element.EFFECTIVEDATE?.["#text"],
+        isInvoice: element.ISINVOICE,
+        masterID: element.MASTERID["#text"],
+        voucherKey: element.VOUCHERKEY["#text"],
+        voucherRetainKey: element.VOUCHERRETAINKEY["#text"],
+        allInventoryEntries: element["ALLINVENTORYENTRIES.LIST"]
+    }
+});
+
+saveOutput({ callerFile: import.meta.url, inData: asSimpleArray });
 console.log("Done");
