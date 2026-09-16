1d614788cef3ff52e8b1716d2dd5e55cc52ed28f
keshavsoft <nknnkumar@yahoo.com>
2026-09-17 00:52:19 +0530

    ledgers

--- Test/v11/asArray/masters/inventory/godown.js
+++ Test/v11/asArray/masters/inventory/godown.js
@@ -1,6 +1,15 @@
-import { masters } from "../../../../src/v4/index.js";
+import { masters } from "../../../../../src/v4/index.js";
 import { saveOutput } from "../../common/index.js";
 
 const data = await masters.inventory.godown();
-saveOutput({ callerFile: import.meta.url, inData: data });
+
+const asSimpleArray = data.GODOWN.map(element => {
+    return {
+        name: element["@_NAME"],
+        reservedName: element["@_RESERVEDNAME"],
+        parent: element.PARENT["#text"]
+    }
+});
+
+saveOutput({ callerFile: import.meta.url, inData: asSimpleArray });
 console.log("Done");
