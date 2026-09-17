4e8b9ccd82ff7dc376ed8e4e6600a4f4d98ebc28
keshavsoft <nknnkumar@yahoo.com>
2026-09-17 16:13:43 +0530

    json start

--- src/v4/masters/inventory/uom/index.js
+++ src/v4/masters/inventory/uom/index.js
@@ -4,28 +4,27 @@ import { fileURLToPath } from "url";
 
 import { executeXml } from "../../../core/index.js";
 import { buildXml } from "../../../core/buildXml.js";
+// import bodyJson from "./body.json" with {type: "json"};
+import bodyJson from "../../../body.json" with {type: "json"};
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const body = fs.readFileSync(path.join(__dirname, "body.xml"), "utf8");
 
 const get = (options = {}) => {
+    const company = options.company;
+    const jsonId = options.jsonId;
+
+    const { tdlMessage, tdlId } = bodyJson[jsonId];
 
     const staticVariables = `
-        <SVCURRENTCOMPANY>${options.company}</SVCURRENTCOMPANY>
-        <SVEXPORTFORMAT>$$SysName:XML</SVEXPORTFORMAT>
-    `;
-
-    const tdlMessage = `
-        <COLLECTION NAME="KeshavUnits">
-            <TYPE>Unit</TYPE>
-            <FETCH>$$Alias:Name</FETCH>
-        </COLLECTION>
-    `;
+    <SVCURRENTCOMPANY>${company}</SVCURRENTCOMPANY>
+    ${bodyJson[jsonId].staticVariables}
+`;
 
     const xml = buildXml(body, {
         staticVariables,
         tdlMessage,
-        tdlId: "KeshavUnits"
+        tdlId
     });
 
     return executeXml(xml, options);
