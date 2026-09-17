b49c7a7032e2c095acb58f6eca398dba8e6bac67
keshavsoft <nknnkumar@yahoo.com>
2026-09-17 19:37:50 +0530

    json perfect

--- src/v5/core/execute/executeXml.js
+++ src/v5/core/execute/executeXml.js
@@ -1,5 +1,5 @@
 // src/v4/core/execute/executeXml.js
-
+import { cleanTallyResponse } from "../cleanTallyResponse.js";
 import { sendXml } from "../transport/http.js";
 import { xmlToJson } from "../response/xmlToJson.js";
 
@@ -9,5 +9,7 @@ export const executeXml = async (
 ) => {
     const rawXml = await sendXml({ xml, url });
 
-    return xmlToJson(rawXml);
+    const json = xmlToJson(rawXml);
+
+    return cleanTallyResponse(json);
 };
