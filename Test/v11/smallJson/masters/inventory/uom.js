603fc269306c0ebcd7c156f9402b53e253dcb347
keshavsoft <nknnkumar@yahoo.com>
2026-09-17 19:29:03 +0530

    now slimmer

--- Test/v11/smallJson/masters/inventory/uom.js
+++ Test/v11/smallJson/masters/inventory/uom.js
@@ -1,6 +1,6 @@
 import v5 from "../../../../../src/v5/index.js";
 import { saveOutput } from "../../common/index.js";
 
-const data = await v5({ company: "mani9", jsonId: "ledgerNames" });
+const data = await v5({ company: "mani9", jsonId: "stockItemsWithBaseUnits" });
 saveOutput({ callerFile: import.meta.url, inData: data });
 console.log("Done");
