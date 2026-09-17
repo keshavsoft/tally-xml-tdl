cc626fbe55fbebb9bed85bc72b74429cee2f73a3
keshavsoft <nknnkumar@yahoo.com>
2026-09-17 10:17:18 +0530

    sales

--- Test/v11/import/sales.js
+++ Test/v11/import/sales.js
@@ -1,6 +1,6 @@
-import { transaction } from "../../../src/v4/index.js";
+import { importData } from "../../../src/v4/index.js";
 import { saveOutput } from "./common/index.js";
 
-const data = await transaction.sales.get({ company: "Mani9" });
+const data = await importData.transaction.sales.get({ company: "Mani9" });
 saveOutput({ callerFile: import.meta.url, inData: data });
 console.log("Done");
