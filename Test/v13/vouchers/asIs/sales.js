cbfafb118ef4c51c8ba0634e1c5632aa7df9a9b9
keshavsoft <nknnkumar@yahoo.com>
2026-09-25 12:56:22 +0530

    19

--- Test/v13/vouchers/asIs/sales.js
+++ Test/v13/vouchers/asIs/sales.js
@@ -1,7 +1,7 @@
 import { vouchers } from "../../../../src/index.js";
 // import { saveOutput } from "../../common/index.js";
 
-const vouchersData = await vouchers.sales.get("mani9", "1-Apr-2026", "1-Apr-2026", "period");
+const vouchersData = await vouchers.sales.period("mani9", "1-Apr-2026", "1-Apr-2026");
 // saveOutput({ callerFile: import.meta.url, inData: vouchersData });
 console.log("vouchersData", vouchersData.ENVELOPE.BODY.DATA);
 
